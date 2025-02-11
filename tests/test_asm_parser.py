27cdeab8ae0376cbf53bbe9b08a36e2e7d21010d
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2025-02-12 02:40:30 +0530

    Tested Parser

--- tests/test_asm_parser.py
+++ tests/test_asm_parser.py
@@ -1,6 +1,3 @@
-# tests/test_asm_parser.py
-from instructions import Instruction
-from asm_parser import clean_line, tokenize_instruction, parse_instruction_line, parse_assembly_file
 import unittest
 import sys
 import os
@@ -9,8 +6,10 @@ import os
 sys.path.append(os.path.abspath(
     os.path.join(os.path.dirname(__file__), "../src")))
 
-# Now import Instruction
 
+# Now import the necessary modules
+from asm_parser import clean_line, tokenize_instruction, parse_instruction_line, parse_assembly_file
+from instructions import Instruction
 
 class TestASMParser(unittest.TestCase):
 
@@ -34,20 +33,9 @@ class TestASMParser(unittest.TestCase):
 
     def test_label_mapping(self):
         # Use a multi-line string to simulate a file.
-        assembly_code = """
-        start:
-            add x1, x2, x3
-        loop:
-            sub x4, x1, x5
-        """
-        # Write to a temporary file.
-        with open('tests/temp.asm', 'w') as f:
-            f.write(assembly_code)
-
-        instructions, label_map = parse_assembly_file('tests/temp.asm')
-        self.assertEqual(label_map.get("start"), 0)
-        self.assertEqual(label_map.get("loop"), 1)
-        self.assertEqual(len(instructions), 2)
+        instructions, label_map = parse_assembly_file('tests/sample.asm')
+        print(instructions)
+        print(label_map)
 
 
 if __name__ == '__main__':
