89f3db447af579da383516f86706cc7b1c01a97b
Karri Sri Saya Sandeep <srisayasandeepkarri@gmail.com>
2025-02-13 23:19:02 +0530

    memory module and core_mem_test added

---
+++ src/memory.py
@@ -0,0 +1,16 @@
+class Memory:
+  def __init__(self,size_in_bytes = 4096):
+    self.size = size_in_bytes
+    self.word = [0] * (size_in_bytes // 4)#// is used as it returns an integer value instead of float
+
+  def read_word(self,address:int)->int:
+    if(address%4 != 0) or address<0 or address>self.size:
+      raise ValueError(f"Invalid address requested : {address}") # Check for valid address
+    index = address//4
+    return self.word[index]
+
+  def write_word(self, address: int, value:int):
+    if(address%4 != 0) or address<0 or address>self.size:
+      raise ValueError(f"Invalid address requested : {address}")
+    index = address//4
+    self.word[index] = value
---
+++ src/test_core_memory.py
@@ -0,0 +1,30 @@
+import unittest
+from memory import Memory
+from src.core import Core
+from instructions import Instruction
+
+class TestCoreMemory(unittest.TestCase):
+  def test_memory_read_write(self):
+    mem = Memory()
+    mem.write_word(0, 1234)
+    self.assertEqual(mem.read_word(0), 1234)
+    mem.write_word(2,1122)
+    self.assertEqual(mem.read_word(2),1122)
+
+  def test_core_registers(self):
+    core = Core(core_number=1)
+    core.set_register_value("x0", 999)
+    self.assertEqual(core.get_register_value("x0"), 0)
+    core.set_register_value("x1", 42)
+    self.assertEqual(core.get_register_value("x1"), 42)
+
+  def test_core_execute_add(self):
+    core = Core(core_number=0)
+    core.set_register_value("x3", 10)
+    core.set_register_value("x4", 15)
+    instr = Instruction(opcode="add", rd="x2", rs1="x3", rs2="x4", original_line="add x2, x3, x4")
+    core.execute_instruction(instr, None, {})
+    self.assertEqual(core.get_register_value("x2"), 25)
+
+if __name__ == '__main__':
+  unittest.main()
