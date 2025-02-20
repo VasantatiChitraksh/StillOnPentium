2ca235c4e4454189b1677c47880af1a665d57bac
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2025-02-20 13:19:25 +0530

    Update bubble_sort.asm

--- tests/bubble_sort.asm
+++ tests/bubble_sort.asm
@@ -1,40 +1,22 @@
 .data
-address: .word -3,2,5,-6,10,23,4,8,11,-4
-.text
-addi x20,x20,600
-li x14 1
-sw x14,0(x20)
-addi x14,x0,5
-sw x14,4(x20)
-addi x14,x0,2
-sw x14,8(x20)
-addi x14,x0,4
-sw x14,12(x20)
-addi x14,x0,7
-sw x14,16(x20)
-addi x14,x0,2
-sw x14,20(x20)
-addi x14,x0,6
-sw x14,24(x20)
-addi x14,x0,8
-sw x14,28(x20)
-addi x14,x0,1
-sw x14,32(x20)
-addi x14,x0,10
-sw x14,36(x20)
+arr:.word 0x1 0x5 0x2 0x4 0x7 0x2 0x6 0x8 0x1 0x0
 
-addi x14,x20,0 #Load base address of array
+.text 
+
+la x14, arr #Load base address of array
 addi x5,x0,10     #n = 10
 addi x10,x0,9   #n = 10 - 1
 addi x11,x0,0    #i = 0
 addi x19,x0,4   #4bytes
 addi x30,x0,0 #k = 0 for printing
 
-loop1:   beq x11,x10,end
+loop1:
+    beq x11,x10,end
     addi x13,x0,0 
     sub x12,x10,x11  #n-1-i
     addi x11,x11,1   #j = 0
-loop2:  beq x13, x12, loop1  # If j == n-1-i, go back to loop1
+loop2:
+    beq x13, x12, loop1  # If j == n-1-i, go back to loop1
     slli x20, x13, 2     # j * 4 (index offset)
     add x21, x14, x20    # Base + offset
     lw x15, 0(x21)       # Load arr[j]
@@ -47,9 +29,10 @@ loop2:  beq x13, x12, loop1  # If j == n-1-i, go back to loop1
     bge x15,x17,if
     addi x13,x13,1
     j loop2
-if:  addi x13,x13,1
+if:
+    addi x13,x13,1
     sw x15,0(x22)
     sw x17,0(x21)
     j loop2
 end:
-    addi x30,x0,1431655765
+    nop
